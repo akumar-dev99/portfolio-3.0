9a178856d71c85a333220b40032f7e9771da06fa
amit2 <amit2@AmitKumar.local>
2025-06-06 02:36:01 +0530

    chore: stop tracking .next folder

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O0Un3mPyjtcq2lCRfs6P4rjCSuFS7VLSKZM0hMXd4Ys=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BYwQYl7nbT5FmSi1FF6vdiIka+5h1LfAJcDOuu06PBA=\"\n}"
